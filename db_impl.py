b643bd7bde2be66c0c9f474ab695df3c63eb959b
Aduuramigba <omosule2ussh@gmail.com>
2025-07-29 13:48:32 +0100

    Query Agent Completion

--- db_impl.py
+++ db_impl.py
@@ -1,14 +1,58 @@
-from agents import agent_output
-from reader import clean_output 
-import asyncio
+
 from pymongo import MongoClient
-from task_runner import start 
-from ui_app import performance_path, kpa_path
+from agents import Agent, function_tool
+from typing import List, Optional
+from pydantic import BaseModel
+
+from reader import INSTRUCTIONS
 
 
-client = MongoClient("localhost", 27017)
 
-db = client.perf
-collection = db.my_data 
+client = MongoClient("mongodb://localhost:27017/")
+db = client.monthly_performance
+collection = db.monthly_performance
 
-collection.insert_one(asyncio.run(start(performance_path,kpa_path)))
+class QueryInput(BaseModel):
+    name: Optional[str] = None
+    month: Optional[List[str]] = None
+
+@function_tool
+def query_database(input: QueryInput) -> list:
+
+    query = {}
+
+    # Add case-insensitive regex filter for 'name'
+    if input.name:
+        query["Employee_Name"] = {"$regex": input.name, "$options": "i"}  # exact match, case-insensitive
+
+    # Match 'month' using $in operator (multiple allowed)
+    if input.month:
+        query["Month_of_Review"] = {"$in": input.month} #exact match, case insensitive
+
+
+    # Debug print statements
+    print("Constructed Query:", query)
+
+    results = list(collection.find(query, {"_id": 0}))
+    return results or ["no matching records found"]
+     
+     
+INSTRUCTIONS1 = f"""you are a query agent, you extract keyword like name (a string) 
+             month (a list of strings, even if user mentions one),
+            from user inputs and retrieve the data specified using {query_database} only use the tool if at least one field is called,
+            and provide an overall analysis on the data only when user specifies it""" 
+
+query_agent = Agent(
+        name= "Query agent",
+        instructions=INSTRUCTIONS1,
+        tools=[query_database],
+         model="gpt-4o-mini",
+   
+
+       )
+
+
+
+     
+
+
--- task_runner.py
+++ task_runner.py
@@ -1,9 +1,10 @@
 from agents import Agent, trace, Runner
 import asyncio
-
 from click import prompt
 
+from db_impl import query_agent
 from reader import filereader, reader_agent
+
 
 async def start(performance_path,kpa_path):
   """run the agents using the file paths provided in the inputs"""
@@ -11,9 +12,15 @@ async def start(performance_path,kpa_path):
    result = await Runner.run(reader_agent, f"Read the excel files {performance_path} and {kpa_path}, and analyse the file ")
   agent_output = result.final_output
   return agent_output
+
+
+async def run_db(prompt):
+  with trace ("querying the database"):
+   result_1 = await Runner.run(query_agent, f"Retrieve the data specified by user's {prompt} and provide overall analysis if users requests it ")
+  return result_1.final_output
+     
   
-  
- 
+
 
  
     
